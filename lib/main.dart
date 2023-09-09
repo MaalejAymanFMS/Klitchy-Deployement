7114cbeb04b12c0728f4bfe4b98646794145d964
GhassenMallek <mallekghassen@gmail.com>
2023-09-10 00:24:08 +0100

    initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:klitchyapp/views/homePage.dart';
 import 'package:klitchyapp/views/splashScreen.dart';
 
 void main() {
@@ -18,8 +17,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      // home:  SplashScreen(),
-      home:  HomePage(),
+      home: SplashScreen(),
     );
   }
 }
