fc3e4e5e67b18a4a41cee80e39e5d4b99e103815
GhassenMallek <mallekghassen@gmail.com>
2023-09-10 17:12:32 +0100

    update home page auth

--- lib/views/homePage.dart
+++ lib/views/homePage.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:klitchyapp/config/app_colors.dart';
+import 'package:klitchyapp/widget/customSwitchButton.dart';
 import 'package:klitchyapp/widget/pinScreen.dart';
 
 class HomePage extends StatefulWidget {
@@ -9,16 +11,50 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  bool isUserLogin = true;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('My App'),
-      ),
-      body: Center(
-       child: Container(
-        child: PinScreen(),
-       ),
+    backgroundColor: AppColors.primaryColor,
+      body: Stack(
+
+        alignment: Alignment.center,
+        children: <Widget>[
+    
+  
+          Positioned(
+            top: 50.0,
+            child: Image.asset(
+              'assets/images/logo.png', 
+              width: 100.0,
+              height: 100.0, 
+            ),
+          ),
+
+          // Switch Button with "USER LOGIN" and "PIN LOGIN" labels
+          Positioned(
+            top: 200.0,
+            child: CustomSwitchButton(
+              initialValue: isUserLogin,
+              onChanged: (bool value) {
+                setState(() {
+                  isUserLogin = value;
+                });
+              },
+            ),
+          ),
+
+         
+          Positioned(
+            top: 300.0, // Adjust vertical position as needed
+            child: isUserLogin
+                ? PinScreen()
+                : PinScreen(),
+          ),
+
+          // Additional Widgets (if needed)
+        ],
       ),
     );
   }
