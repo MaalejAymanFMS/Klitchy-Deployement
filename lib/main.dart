850f3f0ee995376344c1533a8fd9478d291ace0b
GhassenMallek <mallekghassen@gmail.com>
2023-09-18 15:23:22 +0100

    merge rami with ghassen navigation

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:klitchyapp/routes/routes.dart';
 import 'package:klitchyapp/utils/AppState.dart';
 import 'package:klitchyapp/utils/locator.dart';
 import 'package:klitchyapp/views/full_screen_mode.dart';
+import 'package:klitchyapp/views/splashScreen.dart';
 import 'package:provider/provider.dart';
 
 void main() {
@@ -26,7 +27,7 @@ class MyApp extends StatelessWidget {
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
           useMaterial3: true,
         ),
-        home: FullScreenMode(),
+        home: SplashScreen(),
         routes: PageRoutes().routes(),
         debugShowCheckedModeBanner: false,
       ),
