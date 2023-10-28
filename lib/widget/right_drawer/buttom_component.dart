3c624c90079e753ca5337fa10f52765f6162ff0f
MaalejAyman <78954935+MaalejAyman@users.noreply.github.com>
2023-10-28 20:22:57 +0100

    Update buttom_component.dart

--- lib/widget/right_drawer/buttom_component.dart
+++ lib/widget/right_drawer/buttom_component.dart
@@ -8,7 +8,11 @@ import '../../config/app_colors.dart';
 class ButtomComponent extends StatefulWidget {
   final Function() onTap;
   final AppState appState;
-  const ButtomComponent({super.key, required this.onTap, required this.appState,});
+  const ButtomComponent({
+    super.key,
+    required this.onTap,
+    required this.appState,
+  });
 
   @override
   State<ButtomComponent> createState() => _ButtomComponentState();
@@ -26,71 +30,6 @@ class _ButtomComponentState extends State<ButtomComponent> {
       ),
       child: Column(
         children: [
-          Row(
-            children: [
-              Container(
-                width: 191.5.h,
-                height: 41.v,
-                decoration: BoxDecoration(
-                    border: Border.all(
-                  color: AppColors.primaryColor,
-                  width: 1.h,
-                )),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Icon(
-                      Icons.remove_circle_outline,
-                      color: AppColors.pink.withOpacity(0.8),
-                      size: 30.fSize,
-                    ),
-                    SizedBox(
-                      width: 10.h,
-                    ),
-                    Text(
-                      "Discount",
-                      style: TextStyle(color: AppColors.pink.withOpacity(0.8), fontSize: 20.fSize),
-                    )
-                  ],
-                ),
-              ),
-              GestureDetector(
-                onTap: () {
-                  if(widget.appState.checkout) {
-                    widget.appState.switchCheckoutOrder();
-                  } else {
-                    widget.appState.switchCheckout();
-                  }
-                },
-                child: Container(
-                  width: 191.5.h,
-                  height: 41.v,
-                  decoration: BoxDecoration(
-                      border: Border.all(
-                    color: AppColors.primaryColor,
-                    width: 1.h,
-                  )),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Icon(
-                        Icons.add_card_outlined,
-                        color: AppColors.turquoise,
-                        size: 30.fSize,
-                      ),
-                      SizedBox(
-                        width: 10.h,
-                      ),
-                      Text(
-                        "Payment",
-                        style: TextStyle(color: AppColors.turquoise, fontSize: 20.fSize),
-                      )
-                    ],
-                  ),
-                ),
-              ),
-            ],
-          ),
           Padding(
             padding: const EdgeInsets.all(10.0),
             child: Row(
@@ -98,13 +37,17 @@ class _ButtomComponentState extends State<ButtomComponent> {
                 Text(
                   "Subtotal",
                   style: TextStyle(
-                      fontWeight: FontWeight.bold, color: AppColors.textItems, fontSize: 15.fSize),
+                      fontWeight: FontWeight.bold,
+                      color: AppColors.textItems,
+                      fontSize: 15.fSize),
                 ),
                 const Spacer(),
                 Text(
                   "${widget.appState.subtotal.toStringAsFixed(3)} TND",
                   style: TextStyle(
-                      fontWeight: FontWeight.bold, color: AppColors.textItems, fontSize: 15.fSize),
+                      fontWeight: FontWeight.bold,
+                      color: AppColors.textItems,
+                      fontSize: 15.fSize),
                 ),
               ],
             ),
@@ -116,45 +59,114 @@ class _ButtomComponentState extends State<ButtomComponent> {
                 Text(
                   "TVA 7%",
                   style: TextStyle(
-                      fontWeight: FontWeight.bold, color: AppColors.textItems, fontSize: 15.fSize),
+                      fontWeight: FontWeight.bold,
+                      color: AppColors.textItems,
+                      fontSize: 15.fSize),
                 ),
                 const Spacer(),
                 Text(
                   "${widget.appState.tva.toStringAsFixed(3)} TND",
                   style: TextStyle(
-                      fontWeight: FontWeight.bold, color: AppColors.textItems, fontSize: 15.fSize),
+                      fontWeight: FontWeight.bold,
+                      color: AppColors.textItems,
+                      fontSize: 15.fSize),
                 ),
               ],
             ),
           ),
-           Padding(
+          Padding(
             padding: const EdgeInsets.all(10.0),
             child: Row(
               children: [
                 Text(
                   "Total",
                   style: TextStyle(
-                      fontWeight: FontWeight.bold, color: Colors.white, fontSize: 15.fSize),
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                      fontSize: 15.fSize),
                 ),
                 const Spacer(),
                 Text(
                   "${widget.appState.total} TND",
                   style: TextStyle(
-                      fontWeight: FontWeight.bold, color: AppColors.textItems, fontSize: 15.fSize),
+                      fontWeight: FontWeight.bold,
+                      color: AppColors.textItems,
+                      fontSize: 15.fSize),
                 ),
               ],
             ),
           ),
-          const Spacer(),
-          Padding(
-            padding: EdgeInsets.only(left: 20.h, right: 20.h, bottom: 20.v),
-            child: CustomButton(
-              backgroundColor: AppColors.greenColor,
-              icon: "assets/images/arrow_forward.png",
-              text: "Send to kitchen",
-              onTap: widget.onTap,
-            ),
-          )
+          Row(
+            children: [
+              GestureDetector(
+                onTap: () {
+                  widget.onTap();
+                },
+                child: Container(
+                  width: 191.5.h,
+                  height: 70.v,
+                  decoration: BoxDecoration(
+                      border: Border.all(
+                    color: AppColors.primaryColor,
+                    width: 1.h,
+                  )),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(
+                        Icons.arrow_back,
+                        color: AppColors.whiteColor,
+                        size: 30.fSize,
+                      ),
+                      SizedBox(
+                        width: 10.h,
+                      ),
+                      Text(
+                        "Send to kitchen",
+                        style: TextStyle(color: AppColors.whiteColor, fontSize: 20.fSize),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+              GestureDetector(
+                onTap: () {
+                  if (widget.appState.checkout) {
+                    widget.appState.switchCheckoutOrder();
+                  } else {
+                    widget.appState.switchCheckout();
+                  }
+                },
+                child: Container(
+                  width: 191.5.h,
+                  height: 70.v,
+                  decoration: BoxDecoration(
+                      border: Border.all(
+                    color: AppColors.primaryColor,
+                    width: 1.h,
+                  )),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(
+                        Icons.add_card_outlined,
+                        color: AppColors.whiteColor,
+                        size: 30.fSize,
+                      ),
+                      SizedBox(
+                        width: 10.h,
+                      ),
+                      Text(
+                        "Payment",
+                        style: TextStyle(
+                            color: AppColors.whiteColor, fontSize: 20.fSize),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
         ],
       ),
     );
